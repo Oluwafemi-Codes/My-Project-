cfc7482a526b62f21d88bbdb91cdc5f64f21bda2
oluwafemi-Codes <your-peleheh@gmail.com>
2026-08-12 18:35:59 +0100

    Add intro.js assignment

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Your Name";
+const age = 25;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + favoriteLanguage);
+
